4731ddc4261c7f8cd2e167c49e4e9894b4bacba3
Lungabooi <Lungalucasbooi@gmail.com>
2022-10-07 14:46:48 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,96 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constrain"><q-btn 
+        to="/camera"
+        class="large-screen-only q-mr-sm"
+        flat round 
+        icon="eva-camera-outline " 
+        size="18px" 
+        dense
+        
+
+        />
+        <q-separator
+         vertical 
+         inset
+         spaced
+         class="large-screen-only"/> 
+        
+        <q-toolbar-title class="text-grand-hotel text-bold" >
+          Instagram
+        </q-toolbar-title>
+        <q-btn 
+        to="/home"
+        class="large-screen-only"
+        flat round 
+        icon="eva-home-outline " 
+        size="18px" 
+        dense
+
+        />
+
+      </q-toolbar>
+    </q-header> 
+
+    <q-footer 
+    class="bg-white small-screen-only"
+    bordered>
+      
+  <q-tabs 
+   class="text-grey-10"
+   active-color="primary"
+    indicator-color="transparent"      >
+        <q-route-tab
+        to="/home"
+         icon="eva-home-outline" 
+         />
+
+
+        <q-route-tab 
+        to="/camera" 
+        icon="eva-camera-outline" 
+        />
+        
+  
+      </q-tabs>
+        
+      </q-footer>
+
+
+  
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+export default {
+  name: 'MainLayout',
+  
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+  .q-toolbar
+     @media (min-width: $breakpoint-sm-min)
+        height: 77px
+  .q-toolbar__title
+    font-size: 30px
+    @media (max-width: $breakpoint-xs-max)
+      text-align: center
+  .q-footer
+    .q-tab__icon
+      font-size: 40px
+
+
+
+</style>
