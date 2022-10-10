5324266124fbde26ae88d9019591f0c03a5c55c5
Lungabooi <Lungalucasbooi@gmail.com>
2022-10-10 12:21:01 +0200

    card done

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,6 +1,10 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card class="my-card">
+    <q-card 
+    v-for='post in posts'
+     :key="post.id"
+    class="card-post q-mb-md"
+    >
       <q-item>
         <q-item-section avatar>
           <q-avatar>
@@ -10,22 +14,59 @@
 
         <q-item-section>
           <q-item-label>Lunga</q-item-label>
-          <q-item-label caption>Cape Town Western Cape</q-item-label>
+          <q-item-label caption>{{post.location}}</q-item-label>
         </q-item-section>
       </q-item>
 
-      <img src="https://cdn.quasar.dev/img/parallax2.jpg">
+      <img :src="post.imageUrl">
       <q-card-section>
-        <div class="text-h6">Golden Gate Bridge</div>
-        <div class="text-subtitle2">June 10:09 AM</div>
+        <div >{{ post.caption}}</div>
+        <div class="text-caption text-grey">{{post.date | niceDate}}</div>
       </q-card-section>
     </q-card>
   </q-page>
 </template>
 
 <script>
+// import { setServers } from 'dns';
+import { date } from 'quasar'
+
 export default {
-  name: 'PageHome'
+  name: 'PageHome',
+  data() {
+    return {
+      posts: [
+        {
+        id: 1,
+        caption: "Golden Gate Bridge",
+        date: 1665395411362,
+        location: 'Cape Town Western Cape',
+        imageUrl:'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+
+        {
+        id: 2,
+        caption: "Golden Gate Bridge",
+        date: 1665395411362,
+        location: 'Cape Town Western Cape',
+        imageUrl:'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+        {
+        id: 3,
+        caption: "Golden Gate Bridge",
+        date: 1665395411362,
+        location: 'Cape Town Western Cape',
+        imageUrl:'https://cdn.quasar.dev/img/parallax2.jpg'
+        }
+      ]
+    }
+  },
+  filters:{
+    niceDate(value) {
+      return date.formatDate(value, 'MMMM D h:mmA')
+    }
+  }
+
 }
 </script>
 
