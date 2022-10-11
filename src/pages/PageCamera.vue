1c99e2accbeeb185312fd735e4c432201bbd5a41
Lungabooi <Lungalucasbooi@gmail.com>
2022-10-11 11:57:31 +0200

    canvas and video merged using v-show

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -2,13 +2,24 @@
   <q-page class="constrain-more q-pa-md" >
    <div class="camera-frame q-pa-md">
     <video
+    v-show='!imageCaptured'
     ref="video"
     class="full-width"
-    autoplay></video>
+    autoplay
+/>
+<canvas 
+  v-show= 'imageCaptured'
+    ref='canvas'
+    class="full-width"
+    height="240px"
+
+
+    /> 
   
    </div>
    <div class="text-center q-pa-md">
     <q-btn
+    @click="captureImage()"
     color="grey-10"
     icon="eva-camera"
     size="lg"
@@ -56,6 +67,7 @@
 
 <script>
 
+import { nextTick } from 'process'
 import { openURL, uid } from 'quasar'
 export default {
     name: 'PageCamera',
@@ -67,17 +79,26 @@ export default {
           location:'',
           photo:'null',
           date: Date.now()
-        }
+        },
+        imageCaptured: false
       }
     },
     methods: {
       initCamera() {
         navigator.mediaDevices.getUserMedia ( {
-      video: true
-     }). then ( stream => {
+        video: true
+     }). then (stream => {
         this.$refs.video.srcObject = stream
      })
-      }
+      },
+      captureImage() {
+        let video = this.$refs.video
+        let canvas = this.$refs.canvas
+        canvas.width = video.getBoundingClientRect().width
+        canvas.height = video.getBoundingClientRect().height
+        let context = canvas.getContext('2d')
+        context.drawImage(video, 0, 0, canvas.width, canvas.height)
+       this.imageCaptured = true }
     },
     mounted() {
      this.initCamera()
